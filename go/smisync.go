d9a85db4edca0a09f4f9b306061e3d606a68231c
ikpark <ikpark@gmail.com>
2020-10-24 18:56:00 +0900

    go lint

--- go/smisync.go
+++ go/smisync.go
@@ -4,65 +4,59 @@
 package main
 
 import (
-  "bytes"
-  "fmt"
-  "os"
-  "strconv"
-  "strings"
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func usage(cmd string) {
-  fmt.Printf("USAGE: %v <start>+/-<offset> < input.smi > output.smi\n", cmd)
+	fmt.Printf("USAGE: %v <start>+/-<offset> < input.smi > output.smi\n", cmd)
 }
 
-func parse_option(option string) (int, int) {
-  var start, offset, pos int
+func parseOption(option string) (int, int) {
 
-  pos = strings.IndexAny(option, "+-")
-  start,  _ = strconv.Atoi(option[0:pos])
-  offset, _ = strconv.Atoi(option[pos:])
-  if option[pos] == '-' {
-    offset *= -1
-  }
-  return start, offset
+	pos := strings.IndexAny(option, "+-")
+	start, _ := strconv.Atoi(option[0:pos])
+	offset, _ := strconv.Atoi(option[pos:])
+	if option[pos] == '-' {
+		offset *= -1
+	}
+	return start, offset
 }
 
 func main() {
 
-  if len(os.Args) < 2 {
-    usage(os.Args[0])
-    os.Exit(0)
-  }
+	if len(os.Args) < 2 {
+		usage(os.Args[0])
+		os.Exit(0)
+	}
 
-  const syncstr string = "<SYNC Start="
-  var ( // buffer-related vars
-    buf bytes.Buffer
-    line string
-    _e error
-  )
-  var start, offset, sync, tidx, eidx int
+	const syncstr string = "<SYNC Start="
+	var buf bytes.Buffer
 
-  start, offset = parse_option(os.Args[1])
+	start, offset := parseOption(os.Args[1])
 
-  buf.ReadFrom(os.Stdin)
+	buf.ReadFrom(os.Stdin)
 
-  for {
-    line, _e = buf.ReadString('\n');
-    if _e != nil {
-      break
-    }
+	for {
+		line, err := buf.ReadString('\n')
+		if err != nil {
+			break
+		}
 
-    if len(syncstr) <= len(line) && syncstr == line[0:len(syncstr)] {
-      tidx = len(syncstr)
-      eidx = strings.Index(line, ">")
-      sync, _e = strconv.Atoi(line[tidx:eidx])
-      if sync >= start {
-        sync = sync + offset
-      }
+		if len(syncstr) <= len(line) && syncstr == line[0:len(syncstr)] {
+			tidx := len(syncstr)
+			eidx := strings.Index(line, ">")
+			sync, _ := strconv.Atoi(line[tidx:eidx])
+			if sync >= start {
+				sync = sync + offset
+			}
 
-      fmt.Printf("%s%d%s", syncstr, sync, line[eidx:])
-    } else {
-      fmt.Printf("%s", line)
-    }
-  }
+			fmt.Printf("%s%d%s", syncstr, sync, line[eidx:])
+		} else {
+			fmt.Printf("%s", line)
+		}
+	}
 }
